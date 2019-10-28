e8b262aa3094c52c24d3363a9224047875ef145c
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-10-28 15:09:21 -0300

    allows for more than one leaf at each end (and simplifies its drawing)

--- space-colonizer/branch.js
+++ space-colonizer/branch.js
@@ -7,7 +7,8 @@ function Branch(parent, pos, dir, level = 0) {
   this.count = 0;
   this.len = 10;
 
-  this.isLastChild = true;
+  this.howManyLeaves =
+    1 + Math.round(2 * Math.random() * Math.random() * Math.random());
 
   this.reset = function() {
     this.dir = this.origDir.copy();
@@ -23,70 +24,51 @@ function Branch(parent, pos, dir, level = 0) {
     if (dist >= this.len / 4 || this.parent == null) {
       var newLevel = this.level + 1;
       var nextBranch = new Branch(this, nextPos, this.dir.copy(), newLevel);
-      this.isLastChild = false;
+      this.howManyLeaves = 0;
       return nextBranch;
     } else {
       return null;
     }
   };
 
-  this.drawLeaf = function(sketch) {
-    // draws leaft;
-    let leafSize = 4;
+  this.drawLeaf = function(sketch, n = 1, lfW = 4, lfH = 8) {
+    if (n > 0) {
+      lfW = 2 + 2 / n;
+      sketch.push();
+      sketch.translate(this.parent.pos.x, this.parent.pos.y);
 
-    let end = p5.Vector.mult(this.dir, leafSize);
-    end.add(this.pos);
+      let vectAngle;
+      if (this.parent != null) {
+        vectAngle = this.parent.dir.heading();
+      } else {
+        vectAngle = this.dir.heading();
+      }
+      sketch.rotate(vectAngle + sketch.PI / 2);
 
-    // 27 degrees
-    // .559 * leafSize
+      for (let i = 0; i < n; i++) {
+        if (n > 1 && i == 0) {
+          sketch.rotate(-sketch.TWO_PI / 4);
+        }
+        this.drawLeaf(sketch, 0, lfW);
+        sketch.rotate(sketch.TWO_PI / 4);
+      }
 
-    let mid1 = dir.copy();
-    mid1.rotate(-sketch.TWO_PI / 4);
-    mid1.setMag(0.559 * leafSize);
-    mid1.add(this.pos);
+      sketch.pop();
+    } else {
+      sketch.beginShape();
 
-    let cp1_1 = dir.copy();
-    cp1_1.rotate(-sketch.TWO_PI / 4);
-    cp1_1.setMag(0.25 * leafSize);
-    cp1_1.add(this.parent.pos);
-    cp1_1.lerp(mid1, 0.448);
+      sketch.curveVertex(0, 0);
+      sketch.curveVertex(0, 0);
+      sketch.curveVertex(-0.25 * lfW, -0.25 * lfH);
+      sketch.curveVertex(-0.25 * lfW, -0.75 * lfH);
+      sketch.curveVertex(0 * lfW, -1 * lfH);
+      sketch.curveVertex(0.25 * lfW, -0.75 * lfH);
+      sketch.curveVertex(0.25 * lfW, -0.25 * lfH);
+      sketch.curveVertex(0, 0);
+      sketch.curveVertex(0, 0);
 
-    let cp1_2 = dir.copy();
-    cp1_2.rotate(-sketch.TWO_PI / 4);
-    cp1_2.setMag(0.25 * leafSize);
-    cp1_2.add(end);
-    cp1_2.lerp(mid1, 0.448);
-
-    let mid2 = dir.copy();
-    mid2.rotate(sketch.TWO_PI / 4);
-    mid2.setMag(0.559 * leafSize);
-    mid2.add(this.pos);
-
-    let cp2_1 = dir.copy();
-    cp2_1.rotate(sketch.TWO_PI / 4);
-    cp2_1.setMag(0.25 * leafSize);
-    cp2_1.add(this.parent.pos);
-    cp2_1.lerp(mid2, 0.448);
-
-    let cp2_2 = dir.copy();
-    cp2_2.rotate(sketch.TWO_PI / 4);
-    cp2_2.setMag(0.25 * leafSize);
-    cp2_2.add(end);
-    cp2_2.lerp(mid2, 0.448);
-
-    sketch.beginShape();
-    sketch.curveVertex(this.parent.pos.x, this.parent.pos.y);
-    sketch.curveVertex(this.parent.pos.x, this.parent.pos.y);
-    sketch.curveVertex(cp1_1.x, cp1_1.y);
-    sketch.curveVertex(mid1.x, mid1.y);
-    sketch.curveVertex(cp1_2.x, cp1_2.y);
-    sketch.curveVertex(end.x, end.y);
-    sketch.curveVertex(cp2_2.x, cp2_2.y);
-    sketch.curveVertex(mid2.x, mid2.y);
-    sketch.curveVertex(cp2_1.x, cp2_1.y);
-    sketch.curveVertex(this.parent.pos.x, this.parent.pos.y);
-    sketch.curveVertex(this.parent.pos.x, this.parent.pos.y);
-    sketch.endShape();
+      sketch.endShape();
+    }
   };
 
   this.drawFlower = function(sketch) {
@@ -120,13 +102,13 @@ function Branch(parent, pos, dir, level = 0) {
         sketch.strokeWeight(s);
       }
 
-      if (this.isLastChild) {
+      if (this.howManyLeaves > 0) {
         sketch.fill(255);
         sketch.stroke(255);
         if (i % rateOfFlowering == 0) {
           this.drawFlower(sketch);
         } else {
-          this.drawLeaf(sketch);
+          this.drawLeaf(sketch, this.howManyLeaves);
         }
         sketch.noFill();
       } else {
